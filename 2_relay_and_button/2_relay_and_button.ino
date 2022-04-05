09e22a36f0297a55265c5a86682b6240e110f6c7
Victor Hung <victorhung92@gmail.com>
2022-04-05 18:36:22 -0400

    feat: Update LEDs

--- 2_relay_and_button/2_relay_and_button.ino
+++ 2_relay_and_button/2_relay_and_button.ino
@@ -14,13 +14,13 @@ void setup() {
 
 void loop() {
   if (digitalRead(6) == LOW) {
-    int randNumber = random(0, 2);     // Select a random number between 0 and 2 (upper bound 2 is exclusive)
+    int randNumber = random(0, 3);     // Select a random number between 0 and 3 (upper bound 3 is exclusive)
                                        
-    delay(1000);                       // Let's build some suspense
+    delay(500);                       // Let's build some suspense
     
     if (randNumber == 0) {             // If it was 0 then we proceed with writing HIGH
       digitalWrite(8, HIGH);           // turn the relay on (HIGH is the voltage level)
-      delay(1000);                     // wait one second
+      delay(500);                      // wait half a second
       digitalWrite(8, LOW);            // turn the relay off by making the voltage LOW
     }
   }
