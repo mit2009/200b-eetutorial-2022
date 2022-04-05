62335d13e68e25a38e51a7702a1e4f498606aa97
Victor Hung <victorhung92@gmail.com>
2022-04-05 00:18:53 -0400

    fix: change servo to use output 6 to match slides

--- 3_servo/3_servo.ino
+++ 3_servo/3_servo.ino
@@ -13,7 +13,7 @@
 Servo myservo;        // create servo object to control a servo
 
 void setup() {
-  myservo.attach(9);  // attaches the servo on pin 9 to the servo object
+  myservo.attach(6);  // attaches the servo on pin 9 to the servo object
 }
 
 void loop() {
